dda00527a2e919eba4e3669a0623a334b9927636
AngJunYang <handbombex3@gmail.com>
2022-01-23 13:58:36 +0800

    StartMenu

--- Node2D.gd
+++ Node2D.gd
@@ -14,4 +14,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var time = get_tree().get_root().get_node("Game/AnimationPlayer").get_current_animation_position()
-	$Clock.rotation = (time / 240) * 2 * PI
+	$Clock.rotation = (time / 240) * 2 * PI - PI/2
